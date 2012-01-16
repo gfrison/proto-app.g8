8d54abc5fbcbf2ef6f5c4ba893e77ebd6fcfea21
Giancarlo Frison <giancarlo@gfrison.com>
2012-01-16 15:34:42 +0100

    updates

--- src/main/resources/conf/beans.groovy
+++ src/main/resources/conf/beans.groovy
@@ -5,14 +5,17 @@ import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer
 def environment=System.getProperty("environment")?:'development'
 // http://goo.gl/jv3YP Runtime Spring with the Beans DSL
 beans {
+	xmlns context:"http://www.springframework.org/schema/context"
+	context.'annotation-config'()
 	log4jInitialization(MethodInvokingFactoryBean){
 		targetClass="org.springframework.util.Log4jConfigurer"
 		targetMethod="initLogging"
 		arguments=[new java.lang.String("classpath:conf/${environment}/log4j.xml")]
 	}
-	propertyPlaceholderConfigurer(PropertyPlaceholderConfigurer){
-		location="classpath:conf/${environment}/config.properties"
-	}
+	//'gradle run' load 'development' configuration
+	// use the place holder in a single (not double) quote string, ex: 'it would be ${great}'
+	// http://goo.gl/EEstg
+	context.'property-placeholder'(location:'classpath:conf/${environment}/config.properties')
 
 
 }
